24e1c62aee423e61db2b83e70c882390588b6039
Anton <Kyyzia5@gmail.com>
2022-08-23 14:11:43 +0300

    homework_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,18 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "";
-        String saleMonth = "8";
+        String saleMonth = "";
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
+            ResultSet rs = statement
+                    .executeQuery("SELECT MONTH(`subscription_date`) month FROM `PurchaseList` pl ORDER BY`subscription_date` DESC LIMIT 1;");
+            while (rs.next()) {
+                saleMonth = rs.getString("month");
+            }
+
             ResultSet resultSet = statement
-                    .executeQuery("SELECT pl.`course_name`, (COUNT(*)/"+ saleMonth +") `count` FROM `PurchaseList` pl GROUP BY pl.`course_name`;");
+                    .executeQuery("SELECT pl.`course_name`, (COUNT(*)/" + saleMonth + ") `count` FROM `PurchaseList` pl GROUP BY pl.`course_name`;");
             while (resultSet.next()) {
                 String courseName = resultSet.getString("course_name");
                 String average = resultSet.getString("count");
