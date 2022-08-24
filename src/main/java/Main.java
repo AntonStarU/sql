61d508ad55fb995d8d6e63a901ac623d44197d46
Anton <Kyyzia5@gmail.com>
2022-08-24 13:29:13 +0300

    add date

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(new Date());
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "";
